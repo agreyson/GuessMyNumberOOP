2a2967b88896a53660115f4513d07e1e01d736ab
agreyson <amgreyson@gmail.com>
2019-08-26 16:51:40 -0700

    Initial commit.

--- GuessMyNumberOOP/main.swift
+++ GuessMyNumberOOP/main.swift
@@ -1,12 +1,56 @@
-//
 //  main.swift
-//  GuessMyNumberOOP
+
+//  Guess My Number OOP
+
+//  Created by Ann Greyson on 8/21/19.
+//  Copyright © 2019 Greyson. All rights reserved.
+
 //
-//  Created by agreyson on 8/26/19.
-//  Copyright © 2019 agreyson. All rights reserved.
-//
+
+
 
 import Foundation
 
-print("Hello, World!")
+class GuessMyNumberGame {
+    var guesses: Int;
+    var mysteryNumber: Int;
+    
+    init(){
+        self.guesses = 0
+        mysteryNumber = Int.random(in: 1...100)
+    }
+    
+    func playOneRound() {
+        print("I'm thinking of a number between 1 and 100. Guess it! [\(mysteryNumber)]")
+        while let userInput = readLine(){
+            if let guess = Int(userInput){
+                if (guess != mysteryNumber){
+                    if (guess < mysteryNumber){
+                        print("Too low. Try again.")
+                    } else {
+                        print("Too high. Try again.")
+                    }
+                } else {
+                    print("Correct!")
+                    return
+                }
+            } else {
+                print("Invalid entry. Guess again.")
+            }
+        }
+    }
+    
+    func play() {
+        print("Welcome to Guess My Number!")
+        var goAgain = "Y"
+        while goAgain == "Y" {
+            playOneRound()
+            mysteryNumber = Int.random(in: 1...100)
+            print("Enter Y to play again.")
+            goAgain = readLine()!
+        }
+        print("Good-bye!")
+    }
+}
 
+GuessMyNumberGame().play()
